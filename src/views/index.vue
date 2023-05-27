be88880c80ffa8913a40500ad03db79a6e2bf9f0
huangmin <1773287655@qq.com>
2023-05-27 11:53:02 +0800

    add some charts on home page

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="app-container home">
+    <div class="dashboard-container">
+      <component :is="currentRole" />
+    </div>
   </div>
 </template>
 
 <script setup name="Index">
+  import adminDashboard from './dashboard/admin'
 const version = ref('1.0.0')
-
+const currentRole = adminDashboard;
 function goTarget(url) {
   window.open(url, '__blank')
 }
